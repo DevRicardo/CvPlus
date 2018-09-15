fe962807553edea5381f8c3019b0f7fb19ca3ecb
Jose Ricardo <ricardoingenierias@gmail.com>
2018-09-15 13:16:44 -0500

    [UI] Se agregan la seccion tecnologias usadas

--- src/app/componet/pages/home/home.component.ts
+++ src/app/componet/pages/home/home.component.ts
@@ -8,6 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  pictures = [
+    {
+      id: 1,
+      title: 'JavaScript',
+      img: '../../../../assets/logos/javascript.svg'
+    },
+    {
+      id: 2,
+      title: 'Angular',
+      img: '../../../../assets/logos/angular.svg'
+    },
+    {
+      id: 3,
+      title: 'Java',
+      img: '../../../../assets/logos/java.svg'
+    },
+    {
+      id: 4,
+      title: 'Php',
+      img: '../../../../assets/logos/php.svg'
+    },
+    {
+      id: 5,
+      title: 'Spring Boot',
+      img: '../../../../assets/logos/angular.svg'
+    },
+    {
+      id: 6,
+      title: 'Groovy',
+      img: '../../../../assets/logos/java.svg'
+    },
+];
 
   constructor() { }
 
