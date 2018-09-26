4f1b30aa37f484ea2a007113d6811b81683b9f8c
Jose Ricardo <ricardoingenierias@gmail.com>
2018-09-25 21:41:24 -0500

    [UI] Se reestructura la seccion de habilidades

--- src/app/componet/pages/home/home.component.ts
+++ src/app/componet/pages/home/home.component.ts
@@ -9,8 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  color = 'primary';
+  color = 'warn';
   mode = 'determinate';
+  bufferValue = 0;
 
 
   constructor() { }
